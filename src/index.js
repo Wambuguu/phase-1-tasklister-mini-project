bcaa598139dc3efb1772d5dd0afad2d96519a28e
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-27 13:33:27 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); 
+      const taskDescription = document.getElementById("new-task-description").value;
+      const newTask = document.createElement("li");
+      newTask.textContent = taskDescription;
+  
+      taskList.appendChild(newTask);
+  
+      document.getElementById("new-task-description").value = "";
+    });
+  });
+  
